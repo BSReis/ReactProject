b64b310db15363a3b207d28178652a89a8ef2a04
BSReis <75648762+BSReis@users.noreply.github.com>
2022-03-02 00:52:58 +0000

    project updated

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AboutME from './AboutMe';
+import test from './test';
+
+
 import reportWebVitals from './reportWebVitals';
 
+
+const description = {
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita beatae aperiam rerum voluptate temporibus optio esse est consectetur architecto, impedit voluptatem ratione quis id tempora deleniti at qui vel placeat?',
+  name: 'Hi, I am Joana'
+};
+
+
+//calls my image
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +23,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//calls my about section
+ReactDOM.render(
+  <AboutME
+    text={description.text}
+    name={description.name} />,
+  document.getElementById('aboutMe')
+);
+
+
 reportWebVitals();
